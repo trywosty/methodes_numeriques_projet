405a6666e354a0340afc6b18fdffa55208d6a3b4
trywosty <79262942+trywosty@users.noreply.github.com>
2021-04-09 09:58:05 +0000

    Add files via upload

--- q5.py
+++ q5.py
@@ -24,20 +24,30 @@ def q5(t, R, gamma = 0.06, eta = 0.1, sigma = 0.2, n = 10**7, y0 = [10**7-100, 0
             raise ValueError('Les données ne sont pas valides !')
         if not t or not R:
             raise ValueError('Aucune donnée !')
-        R_func = CubicSpline(t,R, bc_type = 'clamped')
+        R_func = CubicSpline(t,R, bc_type='clamped')
         solution = ode45(lambda t_ode, y : seirmodel(t_ode, y, gamma, sigma, eta, R_func), t_span, y0)
         #Dessiner graphique
         pyplot.figure()
-        pyplot.plot(solution.t, solution.y[0,:], label="S")
-        pyplot.plot(solution.t, solution.y[1,:], label="E")
-        pyplot.plot(solution.t, solution.y[2,:], label="X")
-        pyplot.plot(solution.t, solution.y[3,:], label="R")
+        pyplot.plot(solution.t, solution.y[0,:], label="Personnes suceptibles")
+        pyplot.plot(solution.t, solution.y[1,:], label="Personnes exposées")
+        pyplot.plot(solution.t, solution.y[2,:], label="Personnes infectées")
+        pyplot.plot(solution.t, solution.y[3,:], label="Personnes guéries")
         pyplot.plot(t, R, 'ro', label="Données")
         pyplot.legend(loc="best")
-        pyplot.title("Modèle Sir")
+        pyplot.title("Scénario")
         pyplot.xlabel("Temps(t)")
         pyplot.ylabel("Nombre de personnes")
         pyplot.savefig("pic_q5.png")
+        pyplot.figure()
+
+        x = np.linspace(0,400, 4000)
+        pyplot.plot(x, R_func(x), label="Nombre de reproduction de la maladie (R)")
+        pyplot.plot(t, R, 'ro', label="Données")
+
+        pyplot.legend(loc='best')
+        pyplot.title("Évolution du nombre de repoducction de la maladie selon un scénario donné")
+        pyplot.xlabel('Temps')
+        pyplot.savefig('pic_r_q4.png')
         return
     except ValueError as e:
         print('Erreur dans les données : ', e)
@@ -47,8 +57,8 @@ def q5(t, R, gamma = 0.06, eta = 0.1, sigma = 0.2, n = 10**7, y0 = [10**7-100, 0
         return
     
 def test_q5():
-    t = [ 0, 100, 200, 300]
-    R = [ 0.5, 2, 3, 4]
+    t = [ 0, 30, 60, 100, 145, 190, 200, 240, 300, 400]
+    R = [ 2.5, 4.5, 2.8,2,  2.5, 3, 2, 2.5, 3, 1]
     q5(t, R)
     
 test_q5()
